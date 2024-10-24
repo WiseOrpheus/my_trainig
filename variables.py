6c69ec4c8727c88ca644797fd65681bae4235bea
WiseOrpheus <Dielstrong@gmail.com>
2024-10-24 15:11:30 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+course_name = 'Python'
+number_of_completed_RS = 12
+number_of_hours_spent = 1.5
+time_per_task = number_of_hours_spent / number_of_completed_RS
+print('Course:', course_name, 'Total tasks:', number_of_completed_RS, 'Hours spent:', number_of_hours_spent, 'Average completion time:', time_per_task, 'hours')
